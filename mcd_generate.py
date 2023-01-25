ca5798b4a7a3c019e20effd8fba1c5f04f3c1c4a
“JLO64” <64julianlopez@gmail.com>
2023-01-25 14:33:54 -0800

    Update folder structure

--- mcd_generate.py
+++ mcd_generate.py
@@ -1,11 +1,9 @@
-import datetime, pytz, sys, uvicorn, getopt
+import datetime, pytz, sys, getopt
 from io import BytesIO
-from fastapi import FastAPI
-from starlette.responses import StreamingResponse
 from PIL import Image, ImageDraw, ImageFont
 
-blank_screenshot_path = 'mcd_screenshot_edited.jpg'
-screenshot_wCode_path = 'mcd_screenshot_wCode.jpg'
+blank_screenshot_path = 'resources/mcd_screenshot_edited.jpg'
+screenshot_wCode_path = 'resources/mcd_screenshot_wCode.jpg'
 
 class orderInfo:
 	def __init__(self):
@@ -13,22 +11,6 @@ class orderInfo:
 		self.current_hour = 12
 		self.current_minute = 0
 		self.timezone_string = 'US/Pacific'
-
-#http://104.173.174.155:3500/mcd-screenshot?code=1701A&timezone=US/Eastern
-app = FastAPI()
-@app.get("/mcd-screenshot")
-async def send(code = "1701", timezone = "US/Pacific"):
-	apiorder = orderInfo()
-
-	apiorder.code = code
-	apiorder.timezone_string = timezone
-	apiorder = get_time_vars(apiorder)
-
-	image = BytesIO()
-	img = create_screenshot_with_code(apiorder)
-	img.save(image, format='JPEG', quality=80)
-	image.seek(0)
-	return StreamingResponse(image, media_type="image/jpeg")
 
 def get_current_hour_in_12hr_format_for_timezone(timezone_str):
 	global current_hour
@@ -48,8 +30,8 @@ def get_time_vars(order):
 def create_screenshot_with_code(order):
 	img = Image.open(blank_screenshot_path)
 	d1 = ImageDraw.Draw(img)
-	codeFont = ImageFont.truetype('Heebo-Black.ttf', 130)
-	timeFont = ImageFont.truetype('HelveticaNeue-Medium.otf', 49)
+	codeFont = ImageFont.truetype('resources/Heebo-Black.ttf', 130)
+	timeFont = ImageFont.truetype('resources/HelveticaNeue-Medium.otf', 49)
 	width, height = img.size
 	d1.text(( (width * 0.37), (height * 0.20)), order.code, fill =(0, 0, 0),font=codeFont)
 	if (order.current_minute < 10):
@@ -60,7 +42,13 @@ def create_screenshot_with_code(order):
 		d1.text(( (width * 0.047), (height * 0.0268)), str(order.current_hour), fill =(0, 0, 0),font=timeFont)
 	else:
 		d1.text(( (width * 0.025), (height * 0.0268)), str(order.current_hour), fill =(0, 0, 0),font=timeFont)
-	return img
+	
+	#return img in a a bytes-like object
+	imgByteArr = BytesIO()
+	img.save(imgByteArr, format='PNG')
+	imgByteArr = imgByteArr.getvalue()
+	return imgByteArr
+	
 
 def check_if_api_or_cli(argv):
 	try:
@@ -82,15 +70,20 @@ def check_if_api_or_cli(argv):
 			run_cli(opts)
 			sys.exit()
 
+def runfromlambda(code):
+	currentOrder = orderInfo()
+	currentOrder = get_time_vars(currentOrder)
+	currentOrder.code = code
+	return create_screenshot_with_code(currentOrder)
+
 def run_cli(opts):
 	currentOrder = orderInfo()
 	currentOrder = get_time_vars(currentOrder)
+	currentOrder.code = 1701
 
-	for opt, arg in opts:
-		if opt in ("-c", "--code"): currentOrder.code = arg
 
-	#print(str(currentOrder.current_hour) + ":" + str(currentOrder.current_minute))
-	print(currentOrder.code)
+	#for opt, arg in opts:
+	#	if opt in ("-c", "--code"): currentOrder.code = arg
 
 	create_screenshot_with_code(currentOrder).save(screenshot_wCode_path)
 
